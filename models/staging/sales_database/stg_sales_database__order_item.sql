555432e6afb26dd36c3af0e441ae1491044c986d
astachov <dbt_git@outlook.com>
2025-05-18 21:09:23 +0200

    Ajout du suffixe _x aux colonnes de stg_sales_database__order_item

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,10 +1,10 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id,
- order_id,
- product_id,
- seller_id,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
- price as unit_price,
- shipping_cost,
- quantity as item_quantity,
- (price * quantity) + shipping_cost as total_order_item_amount
+select CONCAT(order_id, '_', product_id) AS x_order_item_id,
+ x_order_id,
+ x_product_id,
+ x_seller_id,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS x_picked_up_limited_at,
+ price as x_unit_price,
+ x_shipping_cost,
+ quantity as x_item_quantity,
+ (price * quantity) + shipping_cost as x_total_order_item_amount
 from {{ source('sales_database', 'order_item') }}
