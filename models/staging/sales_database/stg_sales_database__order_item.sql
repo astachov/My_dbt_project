c1f40cb25185a801805bae747d5e62b7f135ece0
astachov <64110894+astachov@users.noreply.github.com>
2025-05-19 18:41:44 +0200

    Ajout du suffixe _y aux colonnes and stg_sales_database__order_item.sql (#6)
    Co-authored-by: astachov <dbt_git@outlook.com>

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,10 +1,10 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id,
- order_id,
- product_id,
- seller_id,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
- price as unit_price,
- shipping_cost,
- quantity as item_quantity,
- (price * quantity) + shipping_cost as total_order_item_amount
+select CONCAT(order_id, '_', product_id) AS y_order_item_id,
+ y_order_id,
+ y_product_id,
+ y_seller_id,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS y_picked_up_limited_at,
+ price as y_unit_price,
+ y_shipping_cost,
+ quantity as y_item_quantity,
+ (price * quantity) + shipping_cost as y_total_order_item_amount
 from {{ source('sales_database', 'order_item') }}
